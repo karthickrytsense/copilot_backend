3720bea264f2ca4a3d897c128bffd8d1aaa74876
karthick-rytsense <karthick@rytsensetech.com>
2026-04-28 14:40:13 +0530

    modified the changes for versal for creating log folder

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,23 +1,10 @@
 import logging
 import os
 
-log_file_name = './log/copilot_chatbot.log'
-os.makedirs(os.path.dirname(log_file_name), exist_ok=True)
-
 logging.basicConfig(
-    filename = log_file_name,
-    level = logging.INFO,
-    filemode = 'a',
-    format = '%(asctime)s - %(levelname)s - %(message)s'
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-logger.propagate = False
-logger.handlers.clear()
-
-main_handler = logging.FileHandler(log_file_name, mode='a', encoding='utf-8')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-main_handler.setFormatter(formatter)
-logger.addHandler(main_handler)
